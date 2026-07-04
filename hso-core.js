c5ea5b93a44942f48cc48e9fcdde25862b2f2670
amwojnar-ops <a.m.wojnarowski@gmail.com>
2026-07-04 12:47:27 +0200

    alex

--- hso-core.js
+++ hso-core.js
@@ -277,7 +277,7 @@ const PLAYERS=[
 // Lista potwierdzonych zgłoszeń 1/8 finału. Nie zawiera treści typów.
 const R16_SUBMITTED_PLAYERS = new Set([
   'Andrzej W.','Paweł','Iwona','Tomek','Łukasz',
-  'Michał','Leszek','Lucas','Ola','Magda','Aldona','Jacek','Justyna','Mariusz','Waldemar'
+  'Michał','Leszek','Lucas','Ola','Magda','Aldona','Jacek','Justyna','Mariusz','Waldemar','Alex'
 ]);
 
 // Status typowania fazy pucharowej.
