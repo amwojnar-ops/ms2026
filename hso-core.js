09a4c4245737c5dbf5e2c20be69c9a286f36e736
amwojnar-ops <a.m.wojnarowski@gmail.com>
2026-07-09 12:06:22 +0200

    mariusz

--- hso-core.js
+++ hso-core.js
@@ -432,7 +432,7 @@ const KNOCKOUT_TIP_ROUNDS = [
 ];
 
 const KNOCKOUT_SUBMISSIONS = {
-    qf: new Set(['Andrzej W.','Łukasz','Lucas','Leszek','Mateusz','Michał','Robert','Waldemar','Justyna','Magda','Tomek','Iwona','Ola'])
+    qf: new Set(['Andrzej W.','Łukasz','Lucas','Leszek','Mateusz','Michał','Robert','Waldemar','Justyna','Magda','Tomek','Iwona','Ola','Mariusz'])
 };
 
 function knockoutTipDotPlayers(){
